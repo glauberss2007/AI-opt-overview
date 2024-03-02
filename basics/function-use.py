4fdf3791e7e825a8b87ac8a9bb727d27e77a2e92
glauberss2007 <glauberss2007@gmail.com>
2024-03-02 07:33:45 -0300

    Added functions call example.

---
+++ basics/function-use.py
@@ -0,0 +1,14 @@
+import essentials
+
+result_sum = essentials.my_sum(3,6)
+result_mul = essentials.my_mult(2,6)
+
+print(result_sum)
+print(result_mul)
+
+instance = essentials.structure()
+instance.age = 10
+instance.name = "Felipe"
+
+print(instance.name)
+print(instance.age)
